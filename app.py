4cd9ca73684d42e120d79072659cd0db25f3b134
Krishnanunni Menon <A-9114@ibsplc.com>
2020-06-15 14:20:21 +0530

    update

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
 from flask import Flask
+from flask import request
+import json
+import requests
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello from Python!"
 
+
+@app.route("/post", methods=["POST"])
+def post():
+    data = request.get_json()
+    print(data)
+    return data
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host='0.0.0.0', port=port)
